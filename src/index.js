93e8b24e9172c0db967c61a5ac111ac0e85c1121
TN <varsha.tn@dxc.com>
2020-08-10 14:29:24 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
+import style from './style.css';
+import SearchMovie from './searchMovie';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component{
+  render(){
+    return(
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovie/>
+      </div>
+    );
+  }
+ 
+}
+
+ReactDOM.render(<Main/>,document.getElementById('root'));
+
